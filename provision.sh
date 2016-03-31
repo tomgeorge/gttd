824a0fea85914e7ad6c2f8fcdf4c72d46c3d4f3a
Tom George <tg82490@gmail.com>
2016-03-30 21:01:31 -0400

    Use an older version of node to get around a fsevent issue

--- provision.sh
+++ provision.sh
@@ -4,7 +4,17 @@
 sudo yum makecache fast
 sudo yum install yum-utils
 sudo package-cleanup --cleandupes
-sudo yum install -y gcc gcc-c++ make openssl-devel wget dos2unix git python2 tmux vim
+sudo yum install -y gcc \
+		    gcc-c++ \
+		    make \
+		    openssl-devel \
+		    wget \
+		    dos2unix \
+		    git \
+		    python2 \
+		    tmux \
+		    vim \
+		    zsh
 
 # install nvm
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
@@ -14,18 +24,19 @@ echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
 source /home/vagrant/.profile
 
 # install latest node, npm
-nvm install node
-nvm use node
+nvm install v4.4.0
+nvm use v4.4.0
 
 # install global dev dependencies
-npm install -g webpack
-npm install -g webpack-dev-server
-npm install -g typings
-npm install -g gulp
-npm install -g tsc
-npm install -g mocha
-npm install -g concurrently
-npm install -g jsonlint
+npm install -g webpack \
+	       webpack-dev-server\
+	       typings \ 
+	       gulp \
+	       tsc \
+	       mocha \
+	       concurrently \
+	       jsonlint \
+	       eslint
 
 # clone
 git clone https://github.com/tomgeorge/gttd || true
