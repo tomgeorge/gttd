ca6f44f1786bd26c8f3278c09c01dd0e09d73a3b
Tom George <tg82490@gmail.com>
2016-04-17 21:30:53 -0400

    Angular Tour of Heroes, looks weird bc of materialize and custom css

--- angular/src/app.component.ts
+++ angular/src/app.component.ts
@@ -1,10 +1,43 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {Hero} from './hero';
+import {HeroService} from './hero-service';
+import {HeroDetailComponent} from './hero-detail.component';
+import './index.css';
+
 let template = `
-                <h1>My Second Angular 2 App</h1>
-                <a class="waves-effect waves-light btn" href="#lol">button</a>
-               `;
+        <div class="container">
+            <h1>{{title}}</h1>
+            <h2>My Heroes</h2>
+            <ul class="heroes">
+                <li *ngFor="#hero of heroes"
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
+                    <span class="badge">{{hero.id}}</span> {{hero.name}}
+                </li>
+            </ul>
+            <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+        </div>`;
+
 @Component({
     selector: 'my-app',
-    template: template
+    template: template,
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent { }
+
+export class AppComponent implements OnInit {
+    title : 'Tour of Heroes';
+    heroes : Hero[];
+    selectedHero: Hero;
+
+    constructor(private _heroService: HeroService)  { }
+
+    getHeroes() {
+        this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit () {
+        this.getHeroes();
+    }
+    onSelect(hero: Hero) { this.selectedHero = hero; }
+}
