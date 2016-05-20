9c273b32eb3a3603067499cbbb8a8d27ce3d9355
Tom George <tg82490@gmail.com>
2016-05-20 00:42:49 -0400

    Removed top heroes from dashboard

--- angular/src/dashboard.component.html.ts
+++ angular/src/dashboard.component.html.ts
@@ -13,11 +13,4 @@ export const dashboardHtml = `
                             <p></p>
                             <p></p>
                             <p></p>
-                            <h3>Top Heroes</h3>
-                                <div class="grid grid-pad">
-                                    <div *ngFor="#hero of heroes" (click)="gotoDetail(hero)" class="col-1-4">
-                                        <div class="module hero">
-                                            {{hero.name}}
-                                        </div>
-                                    </div>
-                                </div>`;
+`;
