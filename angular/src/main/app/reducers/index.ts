7dec964b47733ff4523a3abeca1c673ed3a38e57
Tom George <tg82490@gmail.com>
2016-06-07 20:59:15 -0400

    trying to add redux store

---
+++ angular/src/main/app/reducers/index.ts
@@ -0,0 +1,10 @@
+import * as Redux from 'redux';
+const { combineReducers } = Redux;
+import { RootState } from '../store/configureStore';
+
+
+const rootReducer = combineReducers<RootState>({
+
+});
+
+export default rootReducer;
